98277836a0983152a18aa00d3603d91ea9fccdd1
samuel-langlois <samuel.j.langlois@gmail.com>
2025-03-20 17:39:44 -0600

    4.3 errors within

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from textnode import TextNode, TextType, split_nodes_images
+from textnode import TextNode, TextType, split_nodes_image
 
 def main():
     node = TextNode("this is stuff and things", TextType.BOLD, "https://www.boot.dev")
@@ -7,7 +7,7 @@ def main():
         "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
         TextType.TEXT,
     )
-    print(split_nodes_images(node2))
+    print(split_nodes_image(node2))
 
 if __name__ == "__main__":
     main()
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,26 +1,60 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, markdown_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
+    
     def test_eq(self):
         node = HTMLNode("div", "Hello World", None, {"class": "text"})
         node2 = HTMLNode("div", "Hello World", None, {"class": "text"})
         self.assertEqual(node, node2)
+    
     def test_repr(self):
         node = HTMLNode("div", "Hello World", None, {"class": "text"})
         self.assertEqual(repr(node), "HTMLNode(div, Hello World, None, {'class': 'text'})")
+    
     def test_to_html(self):
         node = HTMLNode("div", "Hello World", None, {"class": "text"})
         self.assertRaises(NotImplementedError, node.to_html)
         # Uncomment the following line when to_html is implemented
         #self.assertEqual(node.to_html(), '<div class="text">Hello World</div>')
+
     def test_props_to_html(self):
         node = HTMLNode("div", "Hello World", None, {"class": "text"})
         self.assertEqual(node.props_to_html(), ' class="text"')
-    def test_none(self):
-        node = HTMLNode("div", "Hello World", None, None)
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
 
 
 if __name__ == "__main__":
